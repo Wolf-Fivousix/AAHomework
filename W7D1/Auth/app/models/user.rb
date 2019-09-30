d5df0a3f9f156e6d6001ced65a5fb3c04b4a0315
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-09-29 23:47:59 -0700

    Add User Model validations file

--- W7D1/Auth/app/models/user.rb
+++ W7D1/Auth/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true, message: { "Password can't be blank, duuh!" }
+    before_validation :ensure_session_token
     
 end
