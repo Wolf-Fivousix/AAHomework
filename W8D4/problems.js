d144c9cdb3dee93f05cfb0d1db83c4a561ffc374
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-10-10 23:42:39 -0700

    Improve W8D4 Dragon solution

--- W8D4/problems.js
+++ W8D4/problems.js
@@ -15,3 +15,38 @@ let roar = function(name)
 }
 
 dragonize(["Wolf", "Bunny"], roar);
+
+function Wolf(name, power, spells)
+{
+  this.name = name;
+  this.power = power;
+  this.spells = spells;
+}
+
+Wolf.prototype.howl = function ()
+{
+  console.log(`${this.name} the wolf goes AaaauuUUUUuUUuuu`);
+};
+
+Wolf.prototype.powerUp = function(boost)
+{
+  this.power += boost;
+};
+
+Wolf.prototype.addSpell = function(spell)
+{
+  this.spells.push(spell);
+};
+
+Wolf.prototype.randomCast = function()
+{
+  const randomSpell = Math.random() * this.spells.length;
+  return this.spells[Math.round(randomSpell) - 1];
+};
+let wolf1 = new Wolf("Black", 100, ["Fire Ball", "Ice Wall"]);
+wolf1.howl();
+wolf1.powerUp(8000);
+console.log(wolf1.power);
+wolf1.addSpell("Death Stride");
+console.log(wolf1.spells);
+console.log(wolf1.randomCast());
