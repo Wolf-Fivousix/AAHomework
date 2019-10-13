f0bf3d261bd6c4215040d189accda495fe68257c
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-10-13 14:34:50 -0700

    Add W9D1 finished Canvas project

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 });
+
+const canvas = document.getElementById("mycanvas");
+canvas.height = 500;
+canvas.width = 500;
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "blue";
+ctx.fillRect(5, 5, 100, 100);
+
+ctx.beginPath();
+ctx.arc(200, 200, 100, 0, 2 * Math.PI);
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.fillStyle = "black";
+// ctx.fill();
+ctx.stroke();
