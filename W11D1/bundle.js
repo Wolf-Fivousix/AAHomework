391286695639a083ad293724b9b57b8ab5341f3b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-10-28 00:34:15 -0700

    Complete W11D1 Giphy exercise

--- W11D1/bundle.js
+++ W11D1/bundle.js
@@ -132,14 +132,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/react.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _giphys_index_item__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./giphys_index_item */ "./components/giphys_index_item.jsx");
-/* harmony import */ var _giphys_index_item__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_giphys_index_item__WEBPACK_IMPORTED_MODULE_1__);
 
 
 function GiphysIndex(_ref) {
   var giphys = _ref.giphys;
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, giphys.map(function (giphy) {
-    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_giphys_index_item__WEBPACK_IMPORTED_MODULE_1___default.a, {
-      key: giphy.id,
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_giphys_index_item__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      id: giphy.id,
       giphy: giphy
     });
   }));
@@ -152,10 +151,22 @@ function GiphysIndex(_ref) {
 /*!******************************************!*\
   !*** ./components/giphys_index_item.jsx ***!
   \******************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return GiphysIndexItem; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/react.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 
+function GiphysIndexItem(_ref) {
+  var giphy = _ref.giphy;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: giphy.images.fixed_height.url
+  });
+}
+;
 
 /***/ }),
 
